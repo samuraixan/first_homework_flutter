c891ab82022279ddf178cfc332eff8ad78d32f3f
samuraixan <samuraikol@mail.ru>
2023-06-05 15:50:35 +0500

    Yangi papkalar qoshildi

---
+++ lib/first.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class First extends StatefulWidget {
+  const First({Key? key}) : super(key: key);
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
