b00ae7a5b1145fbe9c4619d0fe5e426ef0c0db7f
samuraixan <samuraikol@mail.ru>
2023-06-05 16:06:57 +0500

    three papkasi yaratildi

---
+++ lib/three.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Three extends StatefulWidget {
+  const Three({Key? key}) : super(key: key);
+
+  @override
+  State<Three> createState() => _ThreeState();
+}
+
+class _ThreeState extends State<Three> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SKDj'),
+      ),
+      body: Column(
+        children: const [
+          Text('sfkyg'),
+          Text('sfkyg'),
+          Text('sfkyg'),
+          Text('sfkyg'),
+          Text('sfkyg'),
+        ],
+      ),
+    );
+  }
+}
