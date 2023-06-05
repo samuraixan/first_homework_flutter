33d8faf4d1d42c589ef69e4fb720b10d962b9f5a
samuraixan <samuraikol@mail.ru>
2023-06-05 16:53:41 +0500

    Detail sahifasi qoshildi

---
+++ lib/blogs/blog_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlogDetail extends StatefulWidget {
+  const BlogDetail({Key? key}) : super(key: key);
+
+  @override
+  State<BlogDetail> createState() => _BlogDetailState();
+}
+
+class _BlogDetailState extends State<BlogDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
