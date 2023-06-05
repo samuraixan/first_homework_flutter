46ee623ed0791e27d57d26fe71e07539bb7682d2
samuraixan <samuraikol@mail.ru>
2023-06-05 15:40:19 +0500

    Battonlar olib tashlandi

--- lib/main.dart
+++ lib/main.dart
@@ -119,22 +119,6 @@ class HomeworkState extends State<Homework> {
                 onPressed: changeText,
                 child: const Text("CLICK"),
               ),
-              ElevatedButton(
-                onPressed: changeText,
-                child: const Text("CLICK"),
-              ),
-              ElevatedButton(
-                onPressed: changeText,
-                child: const Text("CLICK"),
-              ),
-              ElevatedButton(
-                onPressed: changeText,
-                child: const Text("CLICK"),
-              ),
-              ElevatedButton(
-                onPressed: changeText,
-                child: const Text("CLICK"),
-              ),
             ],
           ),
         ));
